0073de4ff02ff8deabc535b841ecb04552d8f9d4
Seungil Jang <jasi798@gmail.com>
2025-03-08 02:34:49 +0900

    http: 제거

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,7 @@ const fetchToilets = async () => {
       const endIndex = page * pageSize;
       
       const response = await fetch(
-        `http://openapi.seoul.go.kr:8088/${API_KEY}/xml/GeoInfoPublicToiletWGS/${startIndex}/${endIndex}/`
+        `//openapi.seoul.go.kr:8088/${API_KEY}/xml/GeoInfoPublicToiletWGS/${startIndex}/${endIndex}/`
       );
       const xmlText = await response.text();
       const parser = new DOMParser();
